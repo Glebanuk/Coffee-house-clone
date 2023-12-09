19411804f974f7730918950fe501f8ba013a8f6e
Gleb <glebbong12387@gmail.com>
2023-12-09 18:52:35 +0200

    feat: add interacive slider

--- coffe-house/index.js
+++ coffe-house/index.js
@@ -2,6 +2,10 @@ const burgerBtn = document.querySelector(".burger");
 const headerNav = document.querySelector(".header__nav");
 const navLinks = document.querySelectorAll(".nav-item a");
 
+// const slider = document.querySelector(".slider");
+const sliderWrap = document.querySelector(".slider-wrapper");
+
+
 // ---------------------------burger menu behavior----------------------
 
 burgerBtn.addEventListener("click", (e) => {
@@ -32,5 +36,130 @@ navLinks.forEach((link) =>{
 })
 
 // ------------------------------slider------------------------------------
+
+
+document.addEventListener("DOMContentLoaded", function() {
+  const slider = document.querySelector(".slider");
+  const sliderWrapper = document.querySelector(".slider-wrapper");
+  const sliderItemWidth = slider.querySelector(".slider-item").offsetWidth;
+
+  const indicators = document.querySelectorAll(".slider-indicator");
+
+  const btnRight = document.querySelector(".slider__btn-right");
+  const btnLeft = document.querySelector(".slider__btn-left");
+
+  let currentSlide = 0;
+  let intervalId;
+
+  function slideLeft() {
+    const currentLeft = parseInt(slider.style.left) || 0;
+    const nextLeft = currentLeft - sliderItemWidth;
+
+    if (nextLeft <= -slider.clientWidth) {
+      slider.style.left = 0;
+      currentSlide = 0;
+    } else {
+      slider.style.left = nextLeft + "px";
+      currentSlide++;
+    }
+
+    updateIndicators();
+  }
+
+  function slideRight() {
+    const currentLeft = parseInt(slider.style.left) || 0;
+    const nextLeft = currentLeft + sliderItemWidth;
+
+    if (currentSlide === 0) {
+      slider.style.left = -((indicators.length - 1) * sliderItemWidth) + "px";
+      currentSlide = indicators.length - 1;
+    } else {
+      slider.style.left = nextLeft + "px";
+      currentSlide--;
+    }
+
+    updateIndicators();
+  }
+
+  function updateIndicators() {
+    indicators.forEach((indicator, index) => {
+      if (index === currentSlide) {
+        indicator.classList.add("active");
+      } else {
+        indicator.classList.remove("active");
+      }
+    });
+  }
+
+  function startInterval() {
+    intervalId = setInterval(slideLeft, 5000);
+  }
+
+  function pauseInterval() {
+    clearInterval(intervalId);
+  }
+
+  function pauseIndicators() {
+    indicators.forEach((indicator, index) => {
+      if (index === currentSlide) {
+        indicator.classList.add("paused");
+        indicator.style.animationPlayState = "paused";
+      }
+    });
+  }
+
+  function resumeIndicators() {
+    indicators.forEach(indicator => {
+      indicator.classList.remove("paused");
+      indicator.style.animationPlayState = "running";
+    });
+  }
+
+  btnRight.addEventListener("click", function() {
+    slideLeft();
+  });
+
+  btnLeft.addEventListener("click", function() {
+    slideRight();
+  });
+
+  sliderWrapper.addEventListener("mouseenter", function() {
+    pauseInterval();
+    pauseIndicators();
+  });
+
+  sliderWrapper.addEventListener("mouseleave", function() {
+    resumeIndicators();
+    startInterval();
+  });
+
+  let touchStartX;
+
+  sliderWrapper.addEventListener("touchstart", function(e) {
+    pauseInterval();
+    pauseIndicators();
+    touchStartX = e.touches[0].clientX;
+  });
+
+  sliderWrapper.addEventListener("touchend", function(e) {
+    const touchEndX = e.changedTouches[0].clientX;
+    if (touchEndX < touchStartX) {
+      slideLeft();
+    }else{
+      slideRight();
+    }
+    resumeIndicators();
+    startInterval();
+  });
+
+  startInterval();
+});
+
+
+
+
+
+
+
 
 
